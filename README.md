4e74ef96854d4ab4d75d4105242338100ccdbe16
Sidharth R <rsidharth53@gmail.com>
2022-05-21 10:21:09 -0700

    bug fixes

---
+++ README.md
@@ -0,0 +1,5 @@
+# A projectile motion simulation that uses WebAssembly
+
+## How to compile
+1. Install the emscripten compiler toolchain
+2. run "emcc -lembind -o ProjectileMotion.js main.cpp"
--- main.cpp
+++ main.cpp
@@ -2,53 +2,62 @@
 #include "headers\Simulation.h"
 #include "headers\DragProjectileMotion.h"
 
-std::vector<double> xPositions;
-std::vector<double> yPositions;
-double _time;
-int _resolution;
-double coordinates[2];
-double maxHieght;
+class Simulator {
+    private:
+        std::vector<double> xPositions;
+        std::vector<double> yPositions;
+        double _time;
+        int _resolution;
+        double coordinates[2];
+        double maxHieght;
 
-void Simulate(double time, int resolution, double velocity, double angle, double area, double dragCoefficient, double mass) {
-    _time = time;
-    _resolution = resolution;
-    xPositions.clear();
-    yPositions.clear();
-    DragProjectileMotion* simulation = new DragProjectileMotion(time, resolution, velocity, angle, area, dragCoefficient, mass, &xPositions, &yPositions);
-    simulation->run();
-    maxHieght = simulation->maxY;
-}
+    public:
+        Simulator(double time, int resolution, double velocity, double angle, double area, double dragCoefficient, double mass) {
+            _time = time;
+            _resolution = resolution;
+            DragProjectileMotion* simulation = new DragProjectileMotion(time, resolution, velocity, angle, area, dragCoefficient, mass, &xPositions, &yPositions);
+            simulation->run();
+            maxHieght = simulation->maxY;
+        }
 
-void getCurrentPosition(int frame) {
-    double index = _resolution/60.0*frame;
-    double wieghts[2] = {1-(index-(int)index), 1-((int)index+1-index)};
-    coordinates[0] = xPositions[(int)index]*wieghts[0] + xPositions[(int)index + 1]*wieghts[1];
-    coordinates[1] = yPositions[(int)index]*wieghts[0] + yPositions[(int)index + 1]*wieghts[1];
-}
+        void getCurrentPosition(int frame) {
+            double index = _resolution/60.0*frame;
+            double wieghts[2] = {1-(index-(int)index), 1-((int)index+1-index)};
+            coordinates[0] = xPositions[(int)index]*wieghts[0] + xPositions[(int)index + 1]*wieghts[1];
+            coordinates[1] = yPositions[(int)index]*wieghts[0] + yPositions[(int)index + 1]*wieghts[1];
+        }
 
-double getX(int frame) {
-    getCurrentPosition(frame);
-    return coordinates[0];
-}
+        double getX(int frame) {
+            getCurrentPosition(frame);
+            return coordinates[0];
+        }
 
-double getY(int frame) {
-    return coordinates[1];
-}
+        double getY(int frame) {
+            return coordinates[1];
+        }
 
-double getMaxHieght() {
-    return maxHieght;
-}
+        double getMaxHieght() {
+            return maxHieght;
+        }
 
-double getMaxLength() {
-    return xPositions[xPositions.size()-1];
-}
+        double getMaxLength() {
+            return xPositions[xPositions.size()-1];
+        }
+
+        double getXs(int index) {
+            return xPositions[index];
+        }
+
+};
 
 using namespace emscripten;
 EMSCRIPTEN_BINDINGS(projectile_motion) {
-    function("Simulate", &Simulate);
-    function("getX", &getX);
-    function("getY", &getY);
-    function("getMaxHieght", &getMaxHieght);
-    function("getMaxLength", &getMaxLength);
+    class_<Simulator>("Simulator")
+        .constructor<double, int, double, double, double, double, double>()
+        .function("getX", &Simulator::getX)
+        .function("getY", &Simulator::getY)
+        .function("getMaxHieght", &Simulator::getMaxHieght)
+        .function("getMaxLength", &Simulator::getMaxLength)
+        .function("getXs", &Simulator::getXs);
 }
 
